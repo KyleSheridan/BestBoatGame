7606e062f2162d0dc31c787836d46e5fd61d3a5f
Sheridank4 <55580454+Sheridank4@users.noreply.github.com>
2020-05-06 13:51:25 +0100

    Added spawner
    implemented spawn mechanic for enemy ships

---
+++ PirateGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int maxShips;
+
+    public GameObject pirate;
+    public GameObject merchant;
+
+    private GameObject[] spawnPoints;
+
+    private bool spawnable = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int shipCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawn");
+
+        if(shipCount < maxShips && spawnable)
+        {
+            int randNum = Random.Range(0, spawnPoints.Length);
+
+            int randShip = Random.Range(0, 2);
+
+            if(randShip == 0)
+            {
+                Instantiate(merchant, spawnPoints[randNum].transform.position, spawnPoints[randNum].transform.rotation);
+            }
+            else
+            {
+                Instantiate(pirate, spawnPoints[randNum].transform.position, spawnPoints[randNum].transform.rotation);
+            }
+
+            StartCoroutine(SpawnWait(5));
+        }
+    }
+
+    IEnumerator SpawnWait(float waitTime)
+    {
+        spawnable = false;
+
+        yield return new WaitForSeconds(waitTime);
+
+        spawnable = true;
+    }
+}
